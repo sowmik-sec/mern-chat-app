3a20ab49cd8926c850daed4d9855f286741d5484
sowmik-sec <sowmik.sec@gmail.com>
2023-04-20 21:42:36 +0600

    initializing the project with dummy data

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+const app = express();
+const port = process.env.PORT || 5000;
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChat = chats.find((c) => c._id === id);
+  res.send(singleChat);
+});
+
+app.listen(port, () => {
+  console.log(`chat app is running on port ${port}`);
+});
