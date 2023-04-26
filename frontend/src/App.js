0f33f545fa813237d064b9faf145695685cc8236
sowmik-sec <sowmik.sec@gmail.com>
2023-04-26 10:37:22 +0600

    background image set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/chats" Component={ChatPage} />
