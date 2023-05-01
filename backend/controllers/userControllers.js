8b9cb7303bde078c4fb8e624e0263d27a0cbbf46
sowmik-sec <sowmik.sec@gmail.com>
2023-05-01 10:31:52 +0600

    authUser function created

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -34,4 +34,21 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
 module.exports = { registerUser };
